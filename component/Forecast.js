050cbb69d658fc15b8b9d4e806524abc8e6b7cb7
Mail3ee <jate2521@gmail.com>
2021-07-17 23:29:31 +0700

    4 and 5

--- component/Forecast.js
+++ component/Forecast.js
@@ -1,39 +1,54 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
 
 export default function Forecast(props) {
     return (
-        <View >
+        <View style={styles.container}>
+            <Text style={styles.zipCode}>Zip Code</Text>
+            <Text style={styles.zipCode}>{props.zipCode}</Text>
             <Text style={styles.main}>{props.main}</Text>
-            <Text style={styles.description}>{props.description}</Text>
+            <Text style={styles.description}>strupper {props.description}</Text>
             <View>
+                <Text style={styles.country}>{props.country}</Text>
                 <Text style={styles.temp}>{props.temp} 
                     <Text style={styles.tempText}>  °C</Text>
                 </Text>
+                <Image style={styles.icon}
+                source={{
+                    uri: "http://openweathermap.org/img/w/" + props.icon + ".png",
+                }}
+            />
             </View>
-            <Text style={styles.country}>{props.country}</Text>
+            
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    main:{
-        top: -130,
-        color: '#fff',
-        fontSize: 30,
-        textAlign: 'center',
-        fontWeight: 'bold',
-        textShadowColor: 'black',
-        textShadowRadius: 5,
+    container:{
+        marginTop: -50,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
-    description:{
+    main:{
         top: -100,
         color: '#fff',
+        fontSize: 30,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        textShadowColor: 'black',
+        textShadowRadius: 5,
+        textTransform: 'uppercase'
+    },
+    description:{
+        top: -80,
+        color: '#fff',
         textAlign: 'center',
         fontSize: 30,
         fontWeight: 'bold',
         textShadowColor: 'black',
         textShadowRadius: 5,
+        textTransform: 'uppercase'
     },
     temp:{
         justifyContent: 'center',
@@ -43,9 +58,11 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         textShadowColor: 'black',
         textShadowRadius: 5,
+        top: -20,
+        textTransform: 'uppercase'
     },
     country:{
-        top: -150,
+        top: -50,
         justifyContent: 'center',
         textAlign: 'center',
         color: '#fff',
@@ -53,8 +70,25 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         textShadowColor: 'black',
         textShadowRadius: 5,
+        textTransform: 'uppercase'
     },
     tempText:{ 
         fontSize: 30,
+    },
+    icon:{
+        left: 35,
+        width: 120,
+        height: 120,
+    },
+    zipCode: {
+        top: -140,
+        color: '#fff',
+        fontSize: 30,
+        fontWeight: 'bold',
+        textShadowColor: 'black',
+        textShadowRadius: 5,
+        justifyContent: 'center',
+        textAlign: 'center',
+        textTransform: 'uppercase'
     },
 });
--- component/Weather.js
+++ component/Weather.js
@@ -18,6 +18,8 @@ export default function Weather(props) {
                     description: json.weather[0].description,
                     temp: json.main.temp,
                     country: json.sys.country,
+                    icon: json.weather[0].icon,
+                    zipCode: props.zipCode,
                 });
             })
             .catch((error) => {
@@ -29,8 +31,6 @@ export default function Weather(props) {
     return (
         <View>
             <ImageBackground source={require('../bg.jpg')} resizeMode="cover" style={styles.backdrop}>
-                <Text style={styles.zipCode}>Zip Code</Text>
-                <Text style={styles.zipCode_code}>{props.zipCode}</Text>
                 <Forecast {...forecastInfo} />
             </ImageBackground>
                 
@@ -44,21 +44,5 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         width: '100%',
         height: '100%',
-    },
-    zipCode: {
-        top: -140,
-        color: '#fff',
-        fontSize: 30,
-        fontWeight: 'bold',
-        textShadowColor: 'black',
-        textShadowRadius: 5,
-    },
-    zipCode_code: {
-        top: -230,
-        color: '#fff',
-        fontSize: 30,
-        fontWeight: 'bold',
-        textShadowColor: 'black',
-        textShadowRadius: 5,
-    },
+    }
 });
